89760c58697aee15a1758f1934800e22d59d33cb
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2022-07-06 23:56:18 +0430

    Creat window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <GL/glut.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 500
+#define WINDOW_POSITION_X 650
+#define WINDOW_POSITION_Y 250
+#define WINDOW_NAME "Bicycle"
+
+using namespace std;
+
+void displayCB();
+
+int main(int argc, char *argv[]) {
+
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGB);
+
+    glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    glutCreateWindow(WINDOW_NAME);
+
+    glutDisplayFunc(displayCB);
+
+    glutMainLoop();
+
     return 0;
+}
+
+void displayCB() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    //TODO Do some stuff here
+
+    glFlush();
 }
